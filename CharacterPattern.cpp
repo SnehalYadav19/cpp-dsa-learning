edecd539b32c0b919ff87d28e2dcf364077ab61a
Snehal Kumar Yadav <yashrao0222@gmail.com>
2026-09-19 21:59:59 +0530

    Create CharacterPattern.cpp

---
+++ CharacterPattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+
+    int i;
+     while (i<=n) {
+
+        int j = 1;
+        while (j<=n) {
+            char ch = 'A' + i -1;
+            cout<< ch;
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i + 1;
+     }
+
+
+}
