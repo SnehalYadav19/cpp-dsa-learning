4b9d3afaf987ac3c619a8a16612b430ce287504c
Snehal Kumar Yadav <yashrao0222@gmail.com>
2026-09-23 19:52:34 +0530

    Create continue.cpp

---
+++ continue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // int n;
+    // cout<<"Enter the value of n"<<endl;
+    // cin>>n;
+
+    for (int i = 0; i<5; i++) {
+        cout<< "Hiee"<<endl;
+        cout<<"Heyy"<<endl;
+        continue;
+
+        cout<<"Reply toh karde"<<endl;
+
+    }
+}
